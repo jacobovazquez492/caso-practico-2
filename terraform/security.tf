89136a9f0e06c01d57fcdc1bb64b9c5d88d9e629
Jacobo Vaáázquez Lorenzo <jacobovazquez492@comunidadunir.net>
2021-07-17 17:13:14 +0200

    Added deploy Jenkins

--- terraform/security.tf
+++ terraform/security.tf
@@ -24,13 +24,47 @@ resource "azurerm_network_security_group" "sshSecGroup" {
     }
 }
 
+resource "azurerm_network_security_group" "kubemasterSecGroup" {
+    name                = "kubemastertraffic"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "kubemaster ports"
+        priority                   = 1010
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "30000-32767"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "CP2"
+    }
+}
+
 # Vinculamos el security group al interface de red de las distintas máquinas virtuales
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface_security_group_association
 
  resource "azurerm_network_interface_security_group_association" "mastersSecGroupAssociation" {
     count                     = length(var.masters)
     network_interface_id      = azurerm_network_interface.mastersNic[count.index].id
-    network_security_group_id = azurerm_network_security_group.sshSecGroup.id
+    network_security_group_id = azurerm_network_security_group.kubemasterSecGroup.id
 }
 
 resource "azurerm_network_interface_security_group_association" "workersSecGroupAssociation" {
